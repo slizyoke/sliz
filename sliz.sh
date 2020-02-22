c75ba3ede751f65f8df5ad4fb9c8163c5b5906f2
slizyoke <55554740+slizyoke@users.noreply.github.com>
2020-02-22 22:06:57 +0300

    Update sliz.sh

--- sliz.sh
+++ sliz.sh
@@ -1,4 +1,4 @@
-sudo apt-get install python3-pip
-sudo pip3 install termcolor futures requests
-chmod +x sliz.py
-python3 sliz.py --list list.txt
+sudo apt-get install python3-pip;
+sudo pip3 install termcolor futures requests;
+chmod +x sliz.py;
+python3 sliz.py --list list.txt;
